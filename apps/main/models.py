cb3c33639c0826d801aa8691ade43f19c3b8a9db
quantum13 <quant13@gmail.com>
2012-08-18 12:41:43 +1100

    hero model attrs

--- apps/main/models.py
+++ apps/main/models.py
@@ -1,3 +1,35 @@
+#coding: utf-8
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+class Hero(models.Model):
+    user = models.ForeignKey(User)
+
+    avatar_url = models.URLField()
+    html_url = models.URLField()
+    blog = models.URLField()
+    location = models.CharField(max_length=200)
+    hireable = models.BooleanField()
+
+    public_repos = models.IntegerField()
+    public_gists = models.IntegerField()
+    followers = models.IntegerField()
+    following = models.IntegerField()
+
+    attack_github = models.IntegerField()
+    defence_github = models.IntegerField()
+    attentiveness_github = models.IntegerField()
+    charm_github = models.IntegerField()
+
+    attack_own = models.IntegerField()
+    defence_own = models.IntegerField()
+    attentiveness_own = models.IntegerField()
+    charm_own = models.IntegerField()
+
+    race = models.CharField(max_length=100)
+
+    wins = models.IntegerField()
+    losses = models.IntegerField()
+    experience = models.IntegerField()
+
+    last_update = models.CharField(max_length=100)
