3f90e5f8336d60961b6d751bf6e68ca89a52ac57
quantum13 <quant13@gmail.com>
2012-08-19 10:54:46 +1100

    hero main ifo tag

---
+++ apps/helpers/templatetags/hero_main_info.py
@@ -0,0 +1,25 @@
+from django import template
+from django.core.urlresolvers import reverse
+
+register = template.Library()
+
+@register.simple_tag(takes_context=True)
+def hero_main_info(context, hero):
+    return """
+        <h2>%s</h2>
+        %s<br>
+        %s<br><br>
+
+        Attack: %s<br>
+        Defence: %s<br>
+        Attentiveness: %s<br>
+        Charm: %s<br>
+    """ % (
+        hero.login,
+        hero.name,
+        hero.race,
+        hero.get_attack(),
+        hero.get_defence(),
+        hero.get_attentiveness(),
+        hero.get_charm(),
+        )
