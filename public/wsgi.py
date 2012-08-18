ca0c23ac8e10f29acd46bf0be7100de049b6a732
quantum13 <quant13@gmail.com>
2012-08-18 23:28:06 +1100

    coreect settings file

--- public/wsgi.py
+++ public/wsgi.py
@@ -5,7 +5,7 @@ import sys
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hgh.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hgh.local_settings")
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
