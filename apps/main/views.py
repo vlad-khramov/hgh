e4b763af31a9c190e22ee7d5f85eb47c56be8d54
quantum13 <quant13@gmail.com>
2012-08-18 14:54:17 +1100

    simple rating (exp) on main

--- apps/main/views.py
+++ apps/main/views.py
@@ -1,10 +1,14 @@
 #coding: utf-8
 from django.shortcuts import render
+from apps.main.models import Hero
 
 
 def home(request):
 
-    return render(request, 'main/home.html',{})
+    rating_exp = Hero.objects.order_by('-experience')[:20]
+    return render(request, 'main/home.html',{
+        'rating_exp': rating_exp
+    })
 
 
 def login(request):
