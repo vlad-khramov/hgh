866c0dcaa9cac7276f6c7f12893f403bacb8c198
quantum13 <quant13@gmail.com>
2012-08-19 08:27:39 +1100

    test for update race

--- apps/main/tests.py
+++ apps/main/tests.py
@@ -4,28 +4,44 @@ from apps.main.models import Hero, Unit
 
 class SimpleTest(TestCase):
 
-    def setUp(self):
-        self.hero = Hero(
-            attack_github = 2,
-            attack_own = 3
-        )
-
     def test_hero_stats(self):
         """
         tests correct getting stats of hero
         """
-        assert self.hero.get_attack() == 5
+        hero = Hero(
+            attack_github = 2,
+            attack_own = 3
+        )
+        assert hero.get_attack() == 5
 
 
     def test_unit_stats(self):
         """
         tests correct getting stats of hero
         """
+        hero = Hero(
+            attack_github = 2,
+            attack_own = 3
+        )
         unit = Unit(
-            hero = self.hero,
+            hero = hero,
             attack_github = 4
         )
 
         assert unit.get_attack() == 9
+
+
+
+    def test_hero_update_race(self):
+        """tests setting race based on race of units"""
+        hero = Hero()
+        Unit(hero=hero,race='human')
+        Unit(hero=hero,race='elf')
+        Unit(hero=hero,race='elf')
+
+        hero.update_race()
+        print hero.race
+
+        assert hero.race == 'elf'
 
 
