fcd4f79561be03ecf4a1c0623bb041629d719231
quantum13 <quant13@gmail.com>
2012-08-18 11:36:49 +1100

    first settings

--- hgh/settings.py
+++ hgh/settings.py
@@ -1,6 +1,12 @@
-# Django settings for hgh project.
+#coding: utf-8
+import os
 
-DEBUG = True
+
+PROJECT_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__),'..'))
+
+
+
+DEBUG = False
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
@@ -59,7 +65,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = os.path.join(PROJECT_ROOT, "static")
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -67,6 +73,7 @@ STATIC_URL = '/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'templates',  "static"),
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
@@ -90,6 +97,19 @@ TEMPLATE_LOADERS = (
 #     'django.template.loaders.eggs.Loader',
 )
 
+TEMPLATE_CONTEXT_PROCESSORS = (
+    'django.contrib.auth.context_processors.auth',
+    'django.core.context_processors.request',
+    'django.core.context_processors.media',
+    'django.core.context_processors.static',
+    'django.contrib.messages.context_processors.messages',
+
+    'social_auth.context_processors.social_auth_by_name_backends',
+    'social_auth.context_processors.social_auth_backends',
+    'social_auth.context_processors.social_auth_by_type_backends',
+    'social_auth.context_processors.social_auth_login_redirect',
+)
+
 MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -105,7 +125,9 @@ ROOT_URLCONF = 'hgh.urls'
 # Python dotted path to the WSGI application used by Django's runserver.
 WSGI_APPLICATION = 'hgh.wsgi.application'
 
-TEMPLATE_DIRS = ('D:/Dropbox/projects/djangodash/2012/hgh/templates',)
+TEMPLATE_DIRS = (
+    os.path.join(PROJECT_ROOT, 'templates'),
+)
 
 INSTALLED_APPS = (
     'django.contrib.auth',
@@ -118,8 +140,25 @@ INSTALLED_APPS = (
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
-    'main',
+
+    'south',
+    'social_auth',
+
+    'apps.main',
 )
+
+
+AUTHENTICATION_BACKENDS = (
+    'social_auth.backends.contrib.github.GithubBackend',
+    'django.contrib.auth.backends.ModelBackend',
+)
+
+LOGIN_URL       = '/login/'
+LOGIN_ERROR_URL = '/error_url/'
+
+GITHUB_APP_ID = ''
+GITHUB_API_SECRET = ''
+
 
 # A sample logging configuration. The only tangible logging
 # performed by this configuration is to send an email to
