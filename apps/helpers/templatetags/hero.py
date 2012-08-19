66ce11b0343d5607c6afddd3776410e1d9df196b
quantum13 <quant13@gmail.com>
2012-08-19 16:08:13 +1100

    echo level

--- apps/helpers/templatetags/hero.py
+++ apps/helpers/templatetags/hero.py
@@ -6,8 +6,9 @@ register = template.Library()
 @register.simple_tag(takes_context=True)
 def hero(context, hero):
     return """
-        <img src="%(avatar)s" width=18 height=18> <a href="/info/%(login)s">%(login)s</a> <a href="http://github.com/%(login)s">[g]</a>
+        <img src="%(avatar)s" width=18 height=18> <a href="/info/%(login)s">%(login)s <b>[%(level)s]</b></a> <a href="http://github.com/%(login)s"><i>g</i></a>
     """ % {
         'avatar': hero.avatar_url,
-        'login': hero.login
+        'login': hero.login,
+        'level': hero.level,
         }
--- apps/helpers/templatetags/hero_main_info.py
+++ apps/helpers/templatetags/hero_main_info.py
@@ -6,7 +6,7 @@ register = template.Library()
 @register.simple_tag(takes_context=True)
 def hero_main_info(context, hero):
     return """
-        <h2>%s</h2>
+        <h2>%s [%s]</h2>
         %s<br>
         %s<br><br>
 
@@ -19,6 +19,7 @@ def hero_main_info(context, hero):
         Charm: %s<br>
     """ % (
         hero.login,
+        hero.level,
         hero.name,
         hero.race,
         hero.wins,
